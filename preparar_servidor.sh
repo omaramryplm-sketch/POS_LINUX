23a93c4f026f7445c005246781591241560279f3
Omar <omar@omnipos.local>
2026-09-18 10:57:36 -0600

    Feat: Agregar limpieza dos2unix al instalador de servidores

--- preparar_servidor.sh
+++ preparar_servidor.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
+﻿#!/bin/bash
 echo "=========================================="
 echo " 🚀 INSTALADOR AUTOMÁTICO DE SERVIDORES"
 echo "=========================================="
-echo "1. Instalando Docker, Fish Shell y herramientas..."
+echo "1. Instalando Docker, Fish Shell, dos2unix y herramientas..."
 sudo apt update
-sudo apt install -y docker.io docker-compose-v2 fish git curl
+sudo apt install -y docker.io docker-compose-v2 fish git curl dos2unix
 
-echo "2. Configurando permisos de Docker (Evita errores de 'Permission Denied')..."
+echo "2. Limpiando formato de archivos (Windows a Linux)..."
+dos2unix *.sh 2>/dev/null
+
+echo "3. Configurando permisos de Docker (Evita errores de 'Permission Denied')..."
 sudo usermod -aG docker $USER
 
-echo "3. Configurando Git para ignorar bloqueos de Antivirus/Firewalls corporativos..."
+echo "4. Configurando Git para ignorar bloqueos de Antivirus/Firewalls corporativos..."
 git config --global http.sslVerify false
 
-echo "4. Estableciendo Fish como tu consola predeterminada..."
+echo "5. Estableciendo Fish como tu consola predeterminada..."
 sudo chsh -s $(which fish) $USER
 
 echo "=========================================="
